e116904e4b24d40328f97bfb4ca26fb6424a5da0
Allison Renaud <allison.m.renaud@gmail.com>
2022-03-24 16:56:39 +0100

    OfferCard
    -add more details to the card
    -add icons to illustrate the details
    -design modified

--- frontend/src/components/OfferCard/OfferCard.jsx
+++ frontend/src/components/OfferCard/OfferCard.jsx
@@ -1,18 +1,27 @@
 import { Link } from 'react-router-dom';
 import './OfferCard.scss';
 import PropTypes from 'prop-types';
+import IconUsers from '../icons/IconUsers/IconUsers';
+import IconBed from '../icons/IconBed/IconBed';
 
 const OfferCard = ({ offer }) => (
-  <Link to="/locations/:id/offer/:id">
+  <Link to={`/locations/${offer.locationId}/offers/${offer.id}`}>
     <div className="offer-card">
       <div className="offer-card__image flex items-center justify-center">
         <img src={offer.image} alt={offer.title} />
       </div>
-      <div className="offer-card__container flex items-center justify-center">
-        <div>
-          {offer.title}
-          <div className="offer-card__location">
-            {offer.zip_code} {offer.city_name}
+      <div className="offer-card__container">
+        <div className="offer-card__title">{offer.title}</div>
+        <div className="offer-card__location">{offer.city_name}</div>
+        <div className="offer-card__price">
+          {offer.price_ttc}€ <span>par semaine</span>
+        </div>
+        <div className="offer-card__amenities flex">
+          <div className="offer-card__ameneties-item flex items-center">
+            <IconUsers height={16} width={16} /> <div className="offer-card__ameneties-value">{offer.people_capacity}</div>
+          </div>
+          <div className="offer-card__ameneties-item  flex items-center">
+            <IconBed height={20} width={20} /> <div className="offer-card__ameneties-value">{offer.rooms}</div>
           </div>
         </div>
       </div>
