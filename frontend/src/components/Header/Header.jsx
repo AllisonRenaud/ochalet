b79b758f8a7123c6b7e94fa02d8d0bc71d485178
Allison Renaud <allison.m.renaud@gmail.com>
2022-03-28 17:42:07 +0200

    Header WIP
    -add link to logo
    -add dashboard link

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 import './Header.scss';
 
 const Header = ({ isTransparent, isWhite }) => (
-  <header className={`header ${isTransparent ? 'header--transparent' : ''}`}>
+  <header className={`header ${isTransparent ? 'header--transparent' : ''} flex items-center justify-between`}>
     <div className={`header__logo ${isWhite ? 'header__logo--white' : ''}`}>
-      <Logo />
+      <Link to="/locations">
+        <Logo />
+      </Link>
+    </div>
+    <div className="header__menu">
+      <Link to="/dashboard">
+        <div className="header__dashboard-link">Tableau de bord</div>
+      </Link>
     </div>
   </header>
 );
