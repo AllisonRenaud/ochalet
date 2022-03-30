dd2b5fb5ac39879c536c194497bfa8e94c0a86b7
Allison Renaud <allison.m.renaud@gmail.com>
2022-03-30 17:54:44 +0200

    Header
    -each header is diferent depending the role
    -const user mocked to test

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,22 +1,44 @@
+import { Button } from 'antd';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import DashboardAdmin from '../../pages/Dashboard/DashboardAdmin/DashboardAdmin';
+import DashboardSeller from '../../pages/Dashboard/DashboardSeller/DashboardSeller';
+import DashboardUser from '../../pages/Dashboard/DashboardUser/DashboardUser';
 import Logo from '../Logo/Logo';
 import './Header.scss';
 
-const Header = ({ isTransparent, isWhite }) => (
-  <header className={`header ${isTransparent ? 'header--transparent' : ''} flex items-center justify-between`}>
-    <div className={`header__logo ${isWhite ? 'header__logo--white' : ''}`}>
-      <Link to="/locations">
-        <Logo />
-      </Link>
-    </div>
-    <div className="header__menu">
-      <Link to="/dashboard">
-        <div className="header__dashboard-link">Tableau de bord</div>
-      </Link>
-    </div>
-  </header>
-);
+const Header = ({ isTransparent, isWhite }) => {
+  const user = {
+    role: 'seller'
+  };
+
+  return (
+    <header className={`header ${isTransparent ? 'header--transparent' : ''} flex items-center justify-between`}>
+      <div className={`header__logo ${isWhite ? 'header__logo--white' : ''}`}>
+        <Link to="/locations">
+          <Logo />
+        </Link>
+      </div>
+      <div className="header__menu flex flex-row">
+        {user.role === 'user' && (
+          <div className="header__dashboard-link">
+            <Link to="/dashboard/user">Tableau de bord</Link>
+          </div>
+        )}
+        {user.role === 'seller' && (
+          <div className="header__dashboard-link">
+            <Link to="/dashboard/seller">Tableau de bord</Link>
+          </div>
+        )}
+        {user.role === 'admin' && (
+          <div className="header__dashboard-link">
+            <Link to="/dashboard/admin">Tableau de bord</Link>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   isTransparent: PropTypes.bool,
