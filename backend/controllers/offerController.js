208ee29e79b09f8cd797155dd6ca1c1654c93dfe
Allison Renaud <allison.m.renaud@gmail.com>
2022-03-30 19:39:00 +0200

    Offer controller and router
    -createOffer, updateOffer and deleteOffer

--- backend/controllers/offerController.js
+++ backend/controllers/offerController.js
@@ -1,4 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
+const { response } = require("express");
 const prisma = new PrismaClient();
 
 const offerController = {
@@ -20,13 +21,13 @@ const offerController = {
       response.status(500).send(error.message);
     }
   },
-  create: async (request, response) => {
+  createOffer: async (request, response) => {
     try {
-      // TODO: search user with token:  const user = await prisma.user.findUnique();
       const newOffer = await prisma.offer.create({
         data: {
           ...request.body,
           status: "inactive",
+          userId: request.user.id,
         },
       });
       response.status(200).json(newOffer);
@@ -49,6 +50,42 @@ const offerController = {
       return response.status(401).send("Access denied");
     } catch (error) {
       return response.status(500).send(error.message);
+    }
+  },
+  updateOffer: async (request, response) => {
+    try {
+      const offerId = parseInt(request.params.offerId);
+      const newOffer = request.body;
+      const offer = await prisma.offer.update({
+        where: { id: offerId },
+        data: newOffer,
+      });
+
+      response.status(200).json(offer);
+    } catch (error) {
+      response.status(500).send(error.message);
+    }
+  },
+
+  deleteOffer: async (request, response) => {
+    try {
+      const offerId = parseInt(request.params.offerId);
+      const user = request.user;
+
+      const offer = await prisma.offer.findUnique({
+        where: { id: offerId },
+      });
+
+      if (offer.userId === user.id || user.role === "admin") {
+        const offerDeleted = await prisma.offer.delete({
+          where: { id: offerId },
+        });
+        response.status(200).json({ success: "Offer deleted successfully" });
+      } else {
+        response.status(401).json({ error: "Access denied" });
+      }
+    } catch (error) {
+      response.status(500).send(error.message);
     }
   },
 };
--- backend/routes/offerRoute.js
+++ backend/routes/offerRoute.js
@@ -1,16 +1,34 @@
 const offerController = require("../controllers/offerController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const rolesMiddleware = require("../middlewares/rolesMiddleware");
 
 const offerRouter = require("express").Router();
 
 offerRouter.route("/offers/").get(offerController.findAll);
 offerRouter.route("/offers/detail/:offerId/").get(offerController.findById);
-offerRouter.post(offerController.create);
 
 offerRouter.get(
   "/offers/my-offers/",
   authMiddleware,
   offerController.getSellerOffers
+);
+
+offerRouter.post(
+  "/offers/",
+  [authMiddleware, rolesMiddleware(["seller"])],
+  offerController.createOffer
+);
+
+offerRouter.patch(
+  "/offers/:offerId/",
+  [authMiddleware, rolesMiddleware(["seller"])],
+  offerController.updateOffer
+);
+
+offerRouter.delete(
+  "/offers/:offerId/",
+  [authMiddleware, rolesMiddleware(["seller", "admin"])],
+  offerController.deleteOffer
 );
 
 module.exports = offerRouter;
