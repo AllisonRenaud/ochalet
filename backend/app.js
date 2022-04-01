0e7a2993f14fa92275080b51930af5e35383ecc4
Allison Renaud <allison.m.renaud@gmail.com>
2022-04-01 18:51:24 +0200

    add bookingRouter to App

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const authRouter = require("./routes/authRoute");
 const locationRouter = require("./routes/locationRoute");
 const offerRouter = require("./routes/offerRoute");
 const userRouter = require("./routes/userRoute");
+const bookingRouter = require("./routes/bookingRoute");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,5 +14,6 @@ app.use(authRouter);
 app.use(userRouter);
 app.use(locationRouter);
 app.use(offerRouter);
+app.use(bookingRouter);
 
 module.exports = app;
