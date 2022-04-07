3a7a971a8e1c08a5d67d55b1c5f5dfed6a1f5bba
Allison Renaud <allison.m.renaud@gmail.com>
2022-04-07 19:23:49 +0200

    Header
    -add new options in the header if the user is log in or not

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,31 +5,50 @@ import './Header.scss';
 
 const Header = ({ isTransparent, isWhite }) => {
   const user = {
-    role: 'seller'
+    role: 'seller',
+    isConected: true
   };
 
   return (
     <header className={`header ${isTransparent ? 'header--transparent' : ''} flex items-center justify-between`}>
       <div className={`header__logo ${isWhite ? 'header__logo--white' : ''}`}>
-        <Link to="/locations">
+        <Link to="/">
           <Logo />
         </Link>
       </div>
       <div className="header__menu flex flex-row">
-        {user.role === 'user' && (
-          <div className="header__dashboard-link">
-            <Link to="/dashboard/user">Tableau de bord</Link>
-          </div>
+        {user.isConected && (
+          <>
+            {user.role === 'user' && (
+              <div className="header__dashboard-link">
+                <Link to="/dashboard/user">Tableau de bord</Link>
+              </div>
+            )}
+            {user.role === 'seller' && (
+              <div className="header__dashboard-link">
+                <Link to="/dashboard/seller">Tableau de bord</Link>
+              </div>
+            )}
+            {user.role === 'admin' && (
+              <div className="header__dashboard-link">
+                <Link to="/dashboard/admin">Tableau de bord</Link>
+              </div>
+            )}
+            <div className="header__dashboard-link">
+              <Link to="/">Se déconnecter</Link>
+            </div>
+          </>
         )}
-        {user.role === 'seller' && (
-          <div className="header__dashboard-link">
-            <Link to="/dashboard/seller">Tableau de bord</Link>
-          </div>
-        )}
-        {user.role === 'admin' && (
-          <div className="header__dashboard-link">
-            <Link to="/dashboard/admin">Tableau de bord</Link>
-          </div>
+
+        {!user.isConected && (
+          <>
+            <div className="header__dashboard-link">
+              <Link to="/login/">Se connecter</Link>
+            </div>
+            <div className="header__dashboard-link">
+              <Link to="/register/">S'inscrire</Link>
+            </div>
+          </>
         )}
       </div>
     </header>
